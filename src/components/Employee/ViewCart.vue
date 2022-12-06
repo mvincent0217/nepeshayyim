7c98ba8ab655c8bf7eff022102d77e98664af24c
Christian Red Cerojano <CRSC@decatechnologies.com>
2022-12-07 07:35:59 +0800

    designui

---
+++ src/components/Employee/ViewCart.vue
@@ -0,0 +1,106 @@
+<template>
+    <div>
+     <!-- Responsive navbar-->
+
+        <!-- Page content-->
+        <div class="container">
+
+
+            <!--Shopping-->
+            <section class="pt-5 pb-5">
+                   <div class="col-sm-6 mb-3 mb-m-1 order-md-1 text-md-left">
+                    <a class="btn btn-default submit" href="file:///C:/Users/CRSC/Desktop/Food%20ordering%20system%20V2/Home.html#">Continue Ordering</a>
+                  </div>
+              <div class="container">
+                <div class="row w-100">
+                    <div class="col-lg-12 col-md-12 col-12">
+                        <h3 class="display-5 mb-2 text-center">Order Cart</h3>
+                        <p class="mb-5 text-center"></p>
+                        <table id="shoppingCart" class="table table-condensed table-responsive">
+                            <thead>
+                                <tr>
+                                    <th style="width:50%"></th>
+                                    <th style="width:12%">Price</th>
+                                    <th style="width:10%">Quantity</th>
+                                    <th style="width:19%"></th>
+                                    <th style="width:9%">Date</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <!--Product 1-->
+                                <tr>
+                                    <td data-th="Product">
+                                        <div class="row">
+                                            <div class="col-md-3 text-left">
+                                                <img src="https://via.placeholder.com/250x250/5fa9f8/ffffff" alt="" class="img-fluid d-none d-md-block rounded mb-2 shadow ">
+                                            </div>
+                                            <div class="col-md-9 text-left mt-sm-2">
+                                                <h4>Food Name</h4>
+                                                <p class="font-weight-light">Description</p>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td data-th="Price">₱50.00</td>
+                                    <td data-th="Quantity">
+                                        <input type="number" class="form-control form-control-lg text-center" value="1">
+                                    </td>
+                                    <td class="actions" data-th="">
+                                        <div class="text-right">
+                                            <button class="btn btn-white border-secondary bg-white btn-md mb-2">
+                                                🗑️
+                                            </button>
+                                        </div>
+                                    </td>
+                                    <td data-th="Date">12/3/2022</td>
+                                </tr>
+                                <!--Product 1-->
+                                <!--Product 2-->
+                                <tr>
+                                    <td data-th="Product">
+                                        <div class="row">
+                                            <div class="col-md-3 text-left">
+                                                <img src="https://via.placeholder.com/250x250/5fa9f8/ffffff" alt="" class="img-fluid d-none d-md-block rounded mb-2 shadow ">
+                                            </div>
+                                            <div class="col-md-9 text-left mt-sm-2">
+                                                <h4>Food Name</h4>
+                                                <p class="font-weight-light">Description</p>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td data-th="Price">₱50.00</td>
+                                    <td data-th="Quantity">
+                                        <input type="number" class="form-control form-control-lg text-center" value="1">
+                                    </td>
+                                    <td class="actions" data-th="">
+                                        <div class="text-right">
+                                            <button class="btn btn-white border-secondary bg-white btn-md mb-2">
+                                               🗑️
+                                            </button>
+                                        </div>
+                                    </td>
+                                    <td data-th="Date">12/3/2022</td>
+                                </tr>
+                                <!--Product 2-->
+
+                            </tbody>
+                        </table>
+                        <div class="float-right text-right">
+                            <h4>Subtotal:</h4>
+                            <h1>₱100.00</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-4 d-flex align-items-center">
+                    <div class="col-sm-6 order-md-2 text-center">
+                        <a href="file:///C:/Users/CRSC/Desktop/Food%20ordering%20system%20V2/Orders.html#" class="btn btn-primary mb-4 btn-lg pl-5 pr-5 text-md-center">Checkout</a>
+                    </div>
+                    <div class="col-sm-6 mb-3 mb-m-1 order-md-1 text-md-left">
+                      <a class="btn btn-default submit" href="file:///C:/Users/CRSC/Desktop/Food%20ordering%20system%20V2/Home.html#">Continue Ordering</a>
+                    </div>
+                </div>
+            </div>
+            </section>
+            <!--Shopping-->
+        </div>
+    </div>
+  </template>
---
+++ src/components/ViewLogin.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <!-- Responsive navbar-->
+
+        <!-- Page content-->
+        <div class="container">
+            <section class="pt-7 pb-5">
+                <div class="container">
+                    <form>
+                         <br><br><br><br><br><br>
+                         <h1 class="text-center">Login Form</h1><br>
+                             <div class="text-center">
+                                 <input type="text" placeholder="Username" /><br><br>
+                            </div>
+                            <div class="text-center">
+                                <input type="password" placeholder="Password" /><br><br>
+                            </div>
+                            <div class="text-center">
+                                <a class="btn btn-default submit" href="file:///C:/Users/CRSC/Desktop/Food%20ordering%20system%20V2/Home.html#">Log in</a>
+                            </div>
+                    </form>
+                </div>
+            </section>
+        </div>
+    </div>
+</template>
