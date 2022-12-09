85d61a77b492e698e6f79cbd992968957bb00259
Michael Vincent Tible <MVDT@decatechnologies.com>
2022-12-09 10:07:48 +0800

    menu with schedule

--- src/components/Employee/ViewHome.vue
+++ src/components/Employee/ViewHome.vue
@@ -8,12 +8,16 @@
             <div class="text-center">
                 <h3 class="display-5 mb-2 text-center">Goodmorning! {{fullname}}</h3><br><br>
                 <label for ="Date">Date of Order Reservation:</label> 
-                 <input type="date" id="date" class="col-sm-4 mb-3 mb-m-2 order-md-2 text-md-center" name="date"><br>
-                 <label>Schedule Hour: </label>&nbsp;<input type="checkbox"><label for="">12PM</label><input type="checkbox"><label for="">7PM</label>
+                <input type="date" id="date" class="col-sm-4 mb-3 mb-m-2 order-md-2 text-md-center" name="date" v-model="date" @change="showdate"><br>
+                <input type="radio" id="lunch" value="10:30:00" v-model="picked" @change="showhour"/>
+                <label for="two">12PM</label>
+                <input type="radio" id="dinner" value="20:00:00" v-model="picked" @change="showhour" />
+                <label for="two">7PM</label>
             </div>
             <tbody>
                  <!-- Product 1-->
-                <tr v-for="(food, index) in Foods" :key="index">
+                 <tr v-if="!Foods.length">No Menu</tr>
+                <tr v-else v-for="(food, index) in Foods" :key="index">
                     <td data-th="Product">
                         <br>
                         <div class="row">
@@ -55,24 +59,60 @@ export default {
         return{
             fullname: window.localStorage.getItem("user"),
             Foods: [],
+            date: null,
+            date2: null,
+            hour: null,
+            calendar_idx: '',
+            enddate: '',
+            picked: null,
         }
     },
     methods:{
         GetCanteenMenuItems(){
             var convert = require('xml-js');
             axios.post("https://dev-b2b/Decatech/BRM_Canteen_Web/GetCanteenMenuItems").then(response => {
-                var result2 = convert.xml2json(response.data,
+            var result = convert.xml2json(response.data,
                {compact: true, spaces: 4});
-               result2 = JSON.parse(result2);
-               this.Foods = result2.ArrayOfCanteenMenuItem.CanteenMenuItem
-               console.log(result2);
-                console.log(response);
+               console.log(result);
+            })
+        },
+        showdate(){
+            this.date;
+        },
+        showhour(){
+            if(this.picked=="10:30:00"){
+                this.enddate = this.date + " 12:30:00"
+            }else{
+                this.enddate = this.date + " 22:00:00"
+            }
+            this.date2 = this.date + " " + this.picked
+            this.GetCanteenCalendarRecords();
+        },
+        GetCanteenCalendarRecords(){
+            var convert = require('xml-js');
+            axios.post("https://dev-b2b/Decatech/BRM_Canteen_Web/GetCanteenCalendarRecords?startdate="+ this.date2 + "&enddate=" + this.enddate ).then(response => {
+                var result = convert.xml2json(response.data,
+               {compact: true, spaces: 4});
+               result = JSON.parse(result);
+               var calendar_idx = result.ArrayOfCanteenCalendarRecord.CanteenCalendarRecord.Calendar_Idx._text;
+               this.GetCanteenMenu(calendar_idx)
+            })
+        },
+        GetCanteenMenu(id){
+            var convert = require('xml-js');
+            axios.post("https://dev-b2b/Decatech/BRM_Canteen_Web/GetCanteenMenu?calendar_idx=" + id).then(response => {
+                var result = convert.xml2json(response.data,
+               {compact: true, spaces: 4});
+               result = JSON.parse(result);
+                if(result.CanteenMenuSchedule.MenuItemList.CanteenMenuItem != undefined){
+                    this.Foods = result.CanteenMenuSchedule.MenuItemList.CanteenMenuItem;
+                }
             })
         }
 
     },
     created(){
-        this.GetCanteenMenuItems();
+        
     }
 }
 </script>
--- src/components/ViewLogin.vue
+++ src/components/ViewLogin.vue
@@ -29,7 +29,6 @@
 import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-
 Vue.use(VueAxios, axios)
 
 export default {
